343c2ab6e39c68c2672feb702855e6061d4c1f99
msttmt <167951444+msttmt@users.noreply.github.com>
2024-06-16 07:29:23 +0900

    DAY5
    DAY5　まとめ
    ・マイクラ自体がJavaでできているので、MainクラスがJavaPluginクラス（spigotが提供している）で継承されている
    ・ctrl+Bで階層が見れる。

---
+++ Sample/src/main/java/plugin/sample/Main.java
@@ -0,0 +1,21 @@
+
+package plugin.sample;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+
+
+}
